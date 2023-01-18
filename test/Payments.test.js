744d4291d34e45aa598b1169f79e2fca33903ba2
reepov <mix.reep@mail.ru>
2023-01-18 19:33:17 +0300

    first tests

--- test/Payments.test.js
+++ test/Payments.test.js
@@ -11,10 +11,22 @@ describe("Payments", function() {
         const Payments = await ethers.getContractFactory("Payments", acc1)
         payments = await Payments.deploy()
          await payments.deployed()
-        console.log(payments.address)
     })
 
     it("should be deployed", async function(){
-        console.log("success")
+        expect(payments.address).to.be.properAddress
+    })
+
+    it("should have 0 ether by default", async function(){
+        const balance = await payments.currentBalance()
+        expect(balance).to.eq(0)
+    })
+
+    it("should be possible to send funds", async function(){
+        const transact = await payments.connect(acc2).pay("hello from hardhat", {value: 100})
+        await expect(() => transact).to.changeEtherBalances([acc2,payments], [-100, 100])
+        await transact.wait()
+        const newPayment = await payments.getPayment(acc2.address, 0)
+        expect(newPayment.message).to.eq("hello from hardhat")
     })
 })
